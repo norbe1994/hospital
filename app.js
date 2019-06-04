d697fc24a29f54720f183fc560c7e5b78d52896d
Norberto <caceresnorbertoenrique@gmail.com>
2019-06-04 10:02:37 -0500

    conectar a mongoDB

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 // importaciones
 const express = require('express');
+const mongoose = require('mongoose');
 
 // inicializar variables
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//
+// conectar con MongoDB
+mongoose.connect('mongodb://localhost:27017/hospital', (err, res) => {
+  if (err) throw err;
+
+  console.log(`Base de datos: \x1b[32m%s\x1b[0m`, 'online');
+});
+
+// rutas
 app.get('/', (req, res, next) => {
   res.status(200).json({
     miMensaje: '¡Todo salío de maravilla!'
